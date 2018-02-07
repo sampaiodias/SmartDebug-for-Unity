398f7f69fda28595c208183880cae72038296d80
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-02-07 15:28:54 -0200

    First version
    > Supports basic and custom filters

---
+++ Editor/SmartDebugWindow.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SmartDebugWindow : EditorWindow {
+
+    [SerializeField] public static bool isEnabled;
+    [SerializeField] public static bool gameplay;
+    [SerializeField] public static bool audio;
+    [SerializeField] public static bool save;
+    [SerializeField] public static bool design;
+    [SerializeField] public static bool userInterface;
+    [SerializeField] public static bool variables;
+    [SerializeField] public static bool error;
+    [SerializeField] public static bool warning;
+    [SerializeField] public static bool other;    
+    [SerializeField] public static string customFilters;
+
+    [MenuItem("Window/SmartDebug")]
+    static void Init()
+    {
+        InitVars();
+        SmartDebugWindow window = (SmartDebugWindow)EditorWindow.GetWindow(typeof(SmartDebugWindow));
+        window.titleContent.text = "SmartDebug";
+
+        window.Show();
+    }
+
+    private static void InitVars()
+    {
+        isEnabled = EditorPrefs.GetBool("SmartDebugIsEnabled");
+
+        gameplay = EditorPrefs.GetBool("SmartDebugGameplay");
+        audio = EditorPrefs.GetBool("SmartDebugAudio");
+        save = EditorPrefs.GetBool("SmartDebugSave");
+        design = EditorPrefs.GetBool("SmartDebugDesign");
+        userInterface = EditorPrefs.GetBool("SmartDebugUserInterface");
+        variables = EditorPrefs.GetBool("SmartDebugVariables");
+        error = EditorPrefs.GetBool("SmartDebugError");
+        warning = EditorPrefs.GetBool("SmartDebugWarning");
+        other = EditorPrefs.GetBool("SmartDebugOther");
+        customFilters = EditorPrefs.GetString("SmartDebugCustomFilters");
+    }
+
+    private static void SaveChanges()
+    {
+        EditorPrefs.SetBool("SmartDebugIsEnabled", isEnabled);
+
+        if (isEnabled)
+        {
+            EditorPrefs.SetBool("SmartDebugGameplay", gameplay);
+            EditorPrefs.SetBool("SmartDebugAudio", audio);
+            EditorPrefs.SetBool("SmartDebugSave", save);
+            EditorPrefs.SetBool("SmartDebugDesign", design);
+            EditorPrefs.SetBool("SmartDebugUserInterface", userInterface);
+            EditorPrefs.SetBool("SmartDebugVariables", variables);
+            EditorPrefs.SetBool("SmartDebugError", error);
+            EditorPrefs.SetBool("SmartDebugWarning", warning);
+            EditorPrefs.SetBool("SmartDebugOther", other);
+            EditorPrefs.SetString("SmartDebugCustomFilters", customFilters);
+        }
+    }
+
+    private void OnEnable()
+    {
+        InitVars();
+    }
+
+    private void OnDisable()
+    {
+        SaveChanges();
+    }
+
+    private void OnGUI()
+    {
+        //var style = new GUIStyle();
+        //style.normal.textColor = (EditorPrefs.GetBool("SmartDebugIsEnabled") ? new Color(0, 175f / 255f, 46f / 255f) : Color.red);
+        //style.fontStyle = FontStyle.Bold;
+        //GUILayout.Space(5);
+        //GUILayout.Label(" Current Status: " + (EditorPrefs.GetBool("SmartDebugIsEnabled") ? "Enabled" : "Disabled"), style);
+
+        GUILayout.Space(10);
+        isEnabled = EditorGUILayout.Toggle("Enable SmartDebug", isEnabled);        
+
+        GUILayout.Space(5);
+        if (isEnabled)
+        {
+            GUILayout.Label("Log messages to show:", EditorStyles.boldLabel);
+            gameplay = EditorGUILayout.Toggle("Gameplay", gameplay);
+            audio = EditorGUILayout.Toggle("Audio", audio);
+            save = EditorGUILayout.Toggle("Save", save);
+            design = EditorGUILayout.Toggle("Design", design);
+            userInterface = EditorGUILayout.Toggle("User Interface", userInterface);
+            variables = EditorGUILayout.Toggle("Variables", variables);
+            error = EditorGUILayout.Toggle("Error", error);
+            warning = EditorGUILayout.Toggle("Warning", warning);
+            customFilters = EditorGUILayout.TextField(new GUIContent("Custom Filters", "Separate them using , and never use spaces!"), customFilters);
+        }
+
+        //GUILayout.Space(15);
+        //if (GUILayout.Button("Save Changes"))
+        //{
+        //    SaveChanges();          
+        //}
+
+        GUILayout.Space(6);
+        EditorGUILayout.HelpBox("SmartDebug uses Debug.Log() to print messages to the console according to what the user want to see (LogFilters). " +
+            "Logs are NEVER printed outside the UnityEditor. For logs on your build, use Debug.Log instead. " +
+            "You can change what types of log messages you will see on Window/SmartDebug. " +
+            "These settings are kept locally using EditorPrefs.", MessageType.Info);
+    }
+}
---
+++ SmartDebug.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Uses Debug.Log() to print messages to the console according to what the user want to see (LogFilter).
+/// Logs are NEVER printed outside the UnityEditor. For logs on your build, use Debug.Log instead.
+/// You can change what types of log messages you will see on Window/SmartDebug.
+/// These settings are kept locally using EditorPrefs.
+/// </summary>
+public static class SmartDebug {
+
+    /// <summary>
+    /// Prints the message on your console if the LogFilter is enabled on your machine.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="filter"></param>
+    public static void Log(string message, LogFilter filter)
+    {
+#if UNITY_EDITOR
+        if (EditorPrefs.GetBool("SmartDebugIsEnabled") && EditorPrefs.GetBool("SmartDebug" + filter.ToString()))
+        {
+            Debug.Log(message);
+        }
+#endif
+    }
+
+    /// <summary>
+    /// Prints the message on your console if LogFilter.Other is enabled on your machine. 
+    /// </summary>
+    /// <param name="message"></param>
+    public static void Log(string message)
+    {
+        Log(message, LogFilter.Other);
+    }
+
+    /// <summary>
+    /// Prints the message on your console if the registered Custom Filters on your machine contain customFilter.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="customFilter"></param>
+    public static void Log(string message, string customFilter)
+    {
+#if UNITY_EDITOR
+        if (EditorPrefs.GetBool("SmartDebugIsEnabled")) {
+            string[] filters = EditorPrefs.GetString("SmartDebugCustomFilters").Split(',');
+            for (int i = 0; i < filters.Length; i++)
+            {
+                if (filters[i] == customFilter)
+                {
+                    Debug.Log(message);
+                    return;
+                }
+            }
+        }        
+#endif
+    }
+
+    /// <summary>
+    /// Prints the message on your console if the LogFilter is enabled on your machine AND if the registered Custom Filters on your machine contain customFilter.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="filter"></param>
+    /// <param name="customFilter"></param>
+    public static void Log(string message, LogFilter filter, string customFilter)
+    {
+#if UNITY_EDITOR
+        if (EditorPrefs.GetBool("SmartDebugIsEnabled") && EditorPrefs.GetBool("SmartDebug" + filter.ToString()))
+        {
+            Log(message, customFilter);
+        }
+#endif
+    }
+
+    public enum LogFilter
+    {
+        Gameplay,
+        Audio,
+        Save,
+        Design,
+        UserInterface,
+        Variables,
+        Error = 97,
+        Warning = 98,
+        Other = 99
+    }
+}
